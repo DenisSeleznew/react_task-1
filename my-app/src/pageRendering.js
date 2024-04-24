ef51a2efdf115a14b3960f42da517b82103bcd65
Teves <generalpokrichkin@icloud.com>
2024-04-24 21:21:05 +0300

    changing Added date without JSX

--- my-app/src/pageRendering.js
+++ my-app/src/pageRendering.js
@@ -1,29 +1,25 @@
 // import { getTodayDate } from './date';
 import LOGO_IMAGE from './logo.svg';
+import { createElement } from 'react';
 
 export const renderingHTML = (callback) => {
-	const root = document.querySelector('#root');
-	const div = document.createElement('div');
-	div.className = 'App';
-	const header = document.createElement('header');
-	header.className = 'App-header';
-	const imgLogo = document.createElement('img');
-	imgLogo.src = LOGO_IMAGE;
-	imgLogo.className = 'App-logo';
-	imgLogo.alt = 'logo';
-	const paragraph = document.createElement('p');
-	const codeInParagraph = document.createElement('code');
-	codeInParagraph.textContent = `${callback}`;
-	const link = document.createElement('a');
-	link.className = 'App-link';
-	link.href = 'https://reactjs.org';
-	link.target = '_blank';
-	link.rel = 'noopener noreferrer';
-	link.textContent = 'Learn React';
-	return (
-		root.append(div),
-		div.append(header),
-		header.append(imgLogo, paragraph, link),
-		paragraph.append(codeInParagraph)
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: LOGO_IMAGE, className: 'App-logo', alt: 'logo' }),
+			createElement('p'),
+			createElement('code'),
+			callback,
+			createElement('a', {
+				className: 'App-link',
+				href: 'https://reactjs.org',
+				target: '_blank',
+				rel: 'noopener noreferrer',
+			}),
+			'Learn React',
+		),
 	);
 };
